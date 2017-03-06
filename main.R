b9a96e01b4b817425d8c673f652d2041f2bc4393
Rebecca Powell <Rebecca.L.Powell@du.edu>
2017-03-05 21:40:12 -0800

    Set-up RStudio project

---
+++ main.R
@@ -0,0 +1,15 @@
+# Rebecca Powell
+# March 2017  
+
+# Import packages
+library(raster)
+library(sp)
+
+# Source functions
+# source('R/function1.R')
+# source('R/function2.r')
+
+# Load datasets
+# load('data/input-model.rda')
+
+# Then actual commands...
